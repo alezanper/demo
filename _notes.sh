dbf728f32601477ff9db51f79694269c801d0d24
Alexander Benavides Ocampo <Alexander.Benavides.Ocampo@ey.com>
2021-07-21 11:40:48 -0500

    [added] docker support

--- _notes.sh
+++ _notes.sh
@@ -7,8 +7,7 @@ apt-get install -y python python-pip
 pip install flask
 
 # COPY app.py /opt/
-
 FLASK_APP=/opt/app.py flask run --host=0.0.0.0 --port=80
 
-
+docker run -d -p 8080:8080 fabenavideso/simple-app
 
