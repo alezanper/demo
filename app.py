ddc7d07b39f29cb455d2745c7659fd2fdfc06556
fa.benavideso@uniandes.edu.co <fabenavideso@gmail.com>
2021-08-11 17:28:52 -0500

    [test] deployment

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "<h1>My Demo</h1>" + "<p>Simple Python Application for testing purposes.</p>" + "<h3>Hello from: " + socket.gethostname() + "</h3>" + "<button>Button</button>"
+    return "<h1>My Demo</h1>" + "<p>A Simple Python Application for testing purposes.</p>" + "<h3>Hello from: " + socket.gethostname() + "</h3>" + "<button>Button</button>"
 
 @app.route('/<name>')
 def greeting(name):
